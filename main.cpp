7afee0b070060339369244164c1b353435c62e5f
umbc1ok <umbcio@gmail.com>
2023-10-26 00:21:56 +0200

    Need to add operators

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
 
 
-	// przemiennosc dodawania
+	// addition commutativity
 	Vector v1(1, 2, 3);
 	Vector v2(3, 4, 5);
 	Vector v3(1, 2, 3);
@@ -21,7 +21,7 @@ int main() {
 	}
 
 
-	// obliczanie kata pomiedzy wektorami
+	// find the angle between two vectors
 	Vector v5(0, 3, 0);
 	Vector v6(5, 5, 0);
 	std::cout << v5.findAngle(v6) << std::endl;
@@ -31,12 +31,13 @@ int main() {
 	Vector v7(4, 5, 1);
 	Vector v8(4, 1, 3);
 
+    // find a vector perpendicular to v7 and v8
+	Vector v9 = v7.cross(v8);
+	std::cout << v9.x << v9.y << v9.z << std::endl;
 
-
+	// normalize a vector
 	Vector v9(1, 2, 3);
-
 	Vector v10 = v9.normalize();
-
 	std::cout << v10.x << v10.y << v10.z << std::endl;
 
 
